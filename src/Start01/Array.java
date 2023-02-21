802e2b9ed3cdbac43cf1f79327f4ae3a7b4fbb4a
youth <dcancer17@gmail.com>
2023-02-21 21:01:05 +0900

    230221

---
+++ src/Start01/Array.java
@@ -0,0 +1,5 @@
+package Start01;
+
+public class Array {
+
+}
---
+++ src/Start01/Basic01.java
@@ -0,0 +1,10 @@
+package Start01;
+
+public class Basic01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start Now");
+	}
+
+}
